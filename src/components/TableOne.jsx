f0833cd7a4581c0ed4c321a2d236750b1431d13f
Kunal-deve1oper <ghoshkunal106@gmail.com>
2023-12-25 00:49:34 +0530

    updated design

--- src/components/TableOne.jsx
+++ src/components/TableOne.jsx
@@ -24,24 +24,7 @@ const people = [
 export function TableOne() {
   return (
     <>
-      <section className="mx-auto w-full max-w-7xl px-4 py-4">
-        <div className="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
-          <div>
-            <h2 className="text-lg font-semibold">Employees</h2>
-            <p className="mt-1 text-sm text-gray-700">
-              This is a list of all employees. You can add new employees, edit or delete existing
-              ones.
-            </p>
-          </div>
-          <div>
-            <button
-              type="button"
-              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
-              Add new employee
-            </button>
-          </div>
-        </div>
+      <section className="mx-auto w-auto max-w-7xl px-4 py-4">
         <div className="mt-6 flex flex-col">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
