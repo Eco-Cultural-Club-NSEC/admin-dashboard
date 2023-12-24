2364d731701c850cdf8ad43dc2096efb36688422
Kunal-deve1oper <ghoshkunal106@gmail.com>
2023-12-24 23:49:08 +0530

    uploaded dashboard

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { SidebarOne } from './components/SidebarOne'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SidebarOne/>
+    </>
+  )
+}
+
+export default App
